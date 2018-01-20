be30bff04568775b77f7a121fca9d1455cbb8d9c
Victor Alcaraz <vctralcaraz@gmail.com>
2018-01-19 22:42:46 -0800

    Changed folder structure and fixed all relative pathing

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import classes from './Modal.css'
-import Aux from '../../../hoc/Auxiliary'
+import Aux from '../../../hoc/Auxiliary/Auxiliary'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => { 
-    return (
-        <Aux>
-            <Backdrop show={ props.show } clicked={ props.modalClosed }/>
-            <div 
-                className={ classes.Modal }
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}>
-                { props.children }
-            </div>
-        </Aux>
-    )
+class Modal extends Component{
+
+    // checks if show got updated. Won't update OrderSummary(props.children) until show updates. 
+    // this is a performance boost
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+
+    componentWillUpdate() {
+        console.log('[Modal] WillUpdate')
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={ this.props.show } clicked={ this.props.modalClosed }/>
+                <div 
+                    className={ classes.Modal }
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    { this.props.children }
+                </div>
+            </Aux>
+        )
+    }
 }
 
-export default modal
+export default Modal
